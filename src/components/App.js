1e30e9ff1821cbc99bdf34415a27a833648882c3
leliovictor <leliov.vieira@gmail.com>
2022-07-06 03:38:50 -0300

    Added basic archives

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserContext from "../contexts/UserContext";
+import {useState} from "react";
+
+import "../assets/styles/reset.css";
+import "../assets/styles/style.css";
+import LoginPage from "./LoginPage";
+
+export default function App() {
+
+    const [userInfo, setUserInfo] = useState({});
+
+  return (
+    <UserContext.Provider value={{userInfo, setUserInfo}}> 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
+  );
+}
