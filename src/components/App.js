155df98dd0c3bd598c2aa6cd3989fc9a17517d6a
Caio <caiosobreira77@gmail.com>
2022-07-06 11:14:04 -0300

    test commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import {useState} from "react";
 import "../assets/styles/reset.css";
 import "../assets/styles/style.css";
 import LoginPage from "./LoginPage";
+import RegisterPage from "./RegisterPage";
 
 export default function App() {
 
@@ -15,6 +16,7 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />}/>
         </Routes>
       </BrowserRouter>
     </UserContext.Provider>
